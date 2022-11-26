0595fadb3019d0f899ae4ff3b7713f9f43b90705
Divyanshu <divyanshuvishwakarma222@gmail.com>
2022-11-27 01:26:42 +0530

    Creat My Hero Academia

---
+++ script.js
@@ -0,0 +1,13 @@
+let nav = document.querySelector("nav");
+
+
+let val;
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+    }
+    else{
+        nav.classList.remove("sticky");
+    }
+}
